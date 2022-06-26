040fd055f988c585dc2806fe4670c8dfe7e05d95
quynhthu0921 <quynhthu0921@gmail.com>
2022-06-26 11:53:19 +0700

    upload

--- listmusic.js
+++ listmusic.js
@@ -2,121 +2,121 @@ let allMusic = [
   {
     name: "Chìm sâu",
     artist: "RPT MCK feat. Trung Trần (Speed up)",
-    img: "music-1-min-min",
+    img: "img-1",
     src: "music-1"
   },
   {
     name: "Lạc vào trong mơ",
     artist: "Simon C X Wuy (Speed up)",
-    img: "music-2-min",
+    img: "img-2",
     src: "music-2"
   },
   {
     name: "Cà phê",
     artist: "Min (Speed up)",
-    img: "music-3-min",
+    img: "img-3",
     src: "music-3"
   },
   {
     name: "Tình đã đầy một tim",
     artist: "Huyền Tâm Môn (Speed up)",
-    img: "music-4-min",
+    img: "img-4",
     src: "music-4"
   },
   {
     name: "Hông về tình yêu",
     artist: "Khoi Vu (Speed up)",
-    img: "music-5-min",
+    img: "img-5",
     src: "music-5"
   },
   {
     name: "Đã lỡ yêu em nhiều",
     artist: "JustaTee (Speed up)",
-    img: "music-6-min",
+    img: "img-6",
     src: "music-6"
   },
   {
     name: "Em là nhất",
     artist: "Kis x Hoàng KayLee x Yahy (Speed up)",
-    img: "music-7-min",
+    img: "img-7",
     src: "music-7"
   },
   {
     name: "Matchanah",
     artist: "Híu x Bâu (Speed up) ",
-    img: "music-8-min",
+    img: "img-8",
     src: "music-8"
   },
   {
     name: "Gác lại âu lo",
     artist: "Da LAB ft Miu Lê (Speed up)",
-    img: "music-9-min",
+    img: "img-9",
     src: "music-9"
   },
   {
     name: "Tell Ur Mom II ",
     artist: "Gii ft. Winno (Speed up)",
-    img: "music-10-min",
+    img: "img-10",
     src: "music-10"
   },
   {
     name: "See tình",
     artist: "Hoàng Thuỳ Linh (Speed up)",
-    img: "music-11-min",
+    img: "img-11",
     src: "music-11"
   },
   {
     name: "-237°c",
     artist: "Hieuthuhai x Lowna (Speed up)",
-    img: "music-12-min",
+    img: "img-12",
     src: "music-12"
   },
   {
     name: "Đen đá không đường",
     artist: "Amee (Speed up)",
-    img: "music-13-min",
+    img: "img-13",
     src: "music-13"
   },
   {
     name: "Anh đánh rơi người yêu này",
     artist: "Andiez ft. Amee (Speed up)",
-    img: "music-14-min",
+    img: "img-14",
     src: "music-14"
   },
   {
     name: "Tình ca tình ta",
     artist: "Kis (Speed up)",
-    img: "music-15-min",
+    img: "img-15",
     src: "music-15"
   },
   {
     name: "Anh Đã Lạc Vào",
     artist: "Prod Truzg x Green (Speed up)",
-    img: "music-16-min",
+    img: "img-16",
     src: "music-16"
   },
   {
     name: "Rằng em mãi ở bên",
     artist: "Bích Phương (Speed up)",
-    img: "music-17-min",
+    img: "img-17",
     src: "music-17"
   },
   {
     name: "Dại khờ",
     artist: "D'CEE, Anh Thư, CM1X (Speed up)",
-    img: "music-18-min",
+    img: "img-18",
     src: "music-18"
   },
   {
     name: "Hai mươi hai (22)",
     artist: "Amee x Hứa Kim Tuyền (Speed up)",
-    img: "music-19-min",
+    img: "img-19",
     src: "music-19"
   },
   {
     name: "Điên",
     artist: "Tri x orange (Speed up)",
-    img: "music-20-min",
+    img: "img-20",
     src: "music-20"
   },
 ];
